ec6501450721583da83b7eec7681d5df48038466
Marlon <marzonhapacheco@gmail.com>
2024-07-19 17:30:41 -0300

    TIrei o comit antigo

--- cypress/e2e/exemplo.cy.js
+++ cypress/e2e/exemplo.cy.js
@@ -1,33 +1,35 @@
-// describe('Teste página', () => {
-// {
-// it('Teste Página', () => { 
+describe('Teste página', () => {
+{
+it('Teste Página', () => { 
 
-//     cy.visit('https://projeto-c-zeta.vercel.app//')
+    cy.visit('https://projeto-c-zeta.vercel.app//')
 
-//     cy.get('._rotate_1fsck_27 > img')
+    cy.get('._rotate_1fsck_27 > img')
 
-//     cy.type('senai muriaé')
-//     cy.get('#search-submit')
+    cy.type('senai muriaé')
+    cy.get('#search-submit')
 
-// });
+});
 
-// }});
+}});
 
 // Segundo teste
-const{Builder, By} = require('selenium-webdriver');
-    (async function firstScript() {
-    try {
-        let driver = await new Builder ().forBrowser('chrome').build();
-        await driver.get('http://www.google.com/');
-        await driver.getTitle();
-        await driver.mane().setTimeout({implicit:1000});
-        let serchBox = await driver.findElement(By.id('q'));
-        let serchButton = await driver.findElement(By.name(btnk));
-        await serchBox.sendKeys('Prefeitura de muriae');
-        await serchButton.click();
-        let value = await searchBox.getAttribute("value");
-        await driver.quit();
-    }   catch (erro){
-            console.error(erro);
-    }
-})();
+// const{Builder, By} = require('selenium-webdriver');
+//     (async function firstScript() {
+//     try {
+//         let driver = await new Builder ().forBrowser('chrome').build();
+//         await driver.get('http://www.google.com/'); // abre o navegador
+//         await driver.getTitle(); //solicitar inf. do navegador
+//         await driver.manage().setTimeouts({implicit:1000}); // ajusta o timeout
+//         let searchBox = await driver.findElement(By.name('q'));
+//         let searchButton = await driver.findElement(By.name('btnk'));
+//         await searchBox.sendKeys('Prefeitura de muriae');
+//         await searchButton.click();
+//         let searchPSeletivo = await driver.findElement(By.className='l');
+//         await searchPSeletivo.click();
+//         // let value = await searchBox.getAttribute("value");
+//         // await driver.quit();
+//     }   catch (erro){
+//             console.error(erro);
+//     }
+// })();
